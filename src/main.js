0858fffd9669bf91592c6c7388b4ab80f5ab403e
viniciussilva2504 <vinicius.silva2504@gmail.com>
2025-01-22 12:51:17 +0000

    ajustes mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-// Importando o arquivo de estilos principal do projeto //
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    //função para acompanhar rolagem no eixo Y (vertical)
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    //secção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) { // Adicionando um evento de clique para cada botão de aba //
         buttons[i].addEventListener('click', function(botao) { 
             const abaAlvo = botao.target.dataset.tabButton; // Pegando o valor do atributo data-tab-button do botão clicado //
@@ -11,7 +25,28 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active'); 
         });
     }
-});
+    //secção faq accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+})
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 // Função para remover a classe de botão ativo de todos os botões de abas e esconder todas as abas ativas//
 function removeBotaoAtivo () {
     const botaoAtivo = document.querySelectorAll('[data-tab-button]');
